fc16ff56fc48073d8f4a0d125e6ad488b52f1d80
RiegerSimon <Simonrieger2002@gmail.com>
2021-11-25 14:19:45 +0100

    Sprint 3 abgabe (ohne Alarm)

--- app/src/main/java/com/example/fitnesstest/StartWorkAct.java
+++ app/src/main/java/com/example/fitnesstest/StartWorkAct.java
@@ -33,6 +33,8 @@ public class StartWorkAct extends AppCompatActivity {
     private long breakTimeLeftInMillis = START_BREAK_TIME_IN_MILLIS;
     private boolean stateBreakButton=false;
     private int page=2;
+    private boolean atBottom=true;
+    private boolean atTop=false;
 
     Animation btthree, bttfour, ttbone, ttbtwo, alphago;
 
@@ -81,27 +83,31 @@ public class StartWorkAct extends AppCompatActivity {
         startTimer();
 
         btnexercise.setOnClickListener(v -> {
+            if(atTop){
+                Toast toast = Toast.makeText(this, "Workout Finished", Toast.LENGTH_LONG);
+                toast.show();
 
+            }
             if (page<=2){
                 fitoneImage.setImageResource(R.drawable.inclinedumbbell);
                 subintropage.setText("2. Incline Dumbbell Flye\nSet an adjustable bench to a 30°-45° angle, and lie back on it with a dumbbell in each hand. Turn your wrists so your palms face each other. Press the weights straight over your chest, then, keeping a slight bend in your elbows, spread your arms open as if you were going for a big bear hug.");
                 page=3;
-
+                atTop=false;
+                atBottom=false;
             }else if (page==3){
                 fitoneImage.setImageResource(R.drawable.cablecrossover);
                 subintropage.setText("3. Cable Crossover\nStand between two facing cable stations with both pulleys set midway between the top and bottom of the station. Attach a D-handle to each pulley and hold one in each hand. Keep your elbows slightly bent, and step forward so there’s tension on the cables.");
                 page++;
+                atTop=false;
+                atBottom=false;
             }
             else if (page==4){
                 fitoneImage.setImageResource(R.drawable.landmine_press);
                 subintropage.setText("4. Landmine Press\nWedge the end of the barbell into a corner of the room (to avoid damage to the walls, you may have to wrap a towel around it). Load the opposite end with weight and grasp it toward the end of the barbell sleeve with your right hand. Stagger your stance so your left leg is in front. Press the bar straight overhead.");
-                page++;
+                atTop=true;
+                atBottom=false;
             }
-            else if(page==5){
-                Toast toast = Toast.makeText(this, "Workout Finished", Toast.LENGTH_LONG);
-                toast.show();
-                page=4;
-            }
+
 
 
             continueButton.setText("Do Workout");
@@ -176,27 +182,31 @@ public class StartWorkAct extends AppCompatActivity {
 
     public void backToPreviousWorkout(View view) {
 
-        if (page==1+1){
+        if (atBottom){
+            Toast toast = Toast.makeText(this, "Du bist bei der ersten Übung", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        if (page-1==1){
             fitoneImage.setImageResource(R.drawable.chest);
             subintropage.setText("1. Barbell Bench Press\n Grasp the bar just outside shoulder-width and arch your back so there’s space between your lower back and the bench. Pull the bar out of the rack and lower it to your sternum, tucking your elbows about 45° to your sides. When the bar touches your body, drive your feet hard into the floor and press the bar back up.");
-            page--;
+            atBottom=true;
+            atTop=false;
 
-
-        } else if (page==2+1){
+        } else if (page-1==2){
             fitoneImage.setImageResource(R.drawable.inclinedumbbell);
             subintropage.setText("2. Incline Dumbbell Flye\nSet an adjustable bench to a 30°-45° angle, and lie back on it with a dumbbell in each hand. Turn your wrists so your palms face each other. Press the weights straight over your chest, then, keeping a slight bend in your elbows, spread your arms open as if you were going for a big bear hug.");
-
+            atBottom=false;
+            atTop=false;
             page--;
 
-        }else if (page>=3+1){
+        }else if (page-1>=3){
             fitoneImage.setImageResource(R.drawable.cablecrossover);
             subintropage.setText("3. Cable Crossover\nStand between two facing cable stations with both pulleys set midway between the top and bottom of the station. Attach a D-handle to each pulley and hold one in each hand. Keep your elbows slightly bent, and step forward so there’s tension on the cables.");
             page=3;
+            atBottom=false;
+            atTop=false;
         }
-        else if (page==1){
-            Toast toast = Toast.makeText(this, "Du bist bei der 1ten Übung", Toast.LENGTH_LONG);
-            toast.show();
-        }
+
 
         continueButton.setText("Do Workout");
         continueButton.setEnabled(false);
